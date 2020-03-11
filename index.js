6b2b71d4dba3ac51fbd9cd2ccbf80fa4dc26a790
ricardo-ljr <ricardo.leitejr95@gmail.com>
2020-03-11 14:24:34 -0600

    Completed Logger

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code away!
+const express = require("express");
+
+const server = require("./server");
+
+const port = 5000;
+
+server.listen(port, () => {
+  console.log(`\n* listening on http://localhost:${port} *\n`);
+});
